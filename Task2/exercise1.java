60bf349beddae662150f8b21f5b6e0cb50913fb7
Regis Humana <enrique.humana@gmail.com>
2019-11-11 02:18:46 -0400

    Task3

---
+++ Task2/exercise1.java
@@ -0,0 +1,36 @@
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * exercise1
+ */
+public class exercise1 {
+
+    public static void main(String[] args) throws IOException{
+    
+        String input = Files.readString(Path.of("example.txt"));
+        
+        String regex = "^\\(?(\\d{2,4})\\)?[\\.\\-\\s]?(\\d{2,4})[\\.\\-\\s]?(\\d{3})[\\.\\-\\s]?(\\d{4})$";
+        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        
+        Matcher matcher = pattern.matcher(input);
+
+        HashSet<String> codes = new HashSet<>();
+        HashSet<String> cities = new HashSet<>();
+        HashSet<String> phones = new HashSet<>();        
+
+        while(matcher.find()) {
+            codes.add(matcher.group(1));
+            cities.add(matcher.group(2));
+            phones.add(matcher.group(3) + matcher.group(4));
+        }
+        System.out.println("Operator codes: " + codes);
+        System.out.println("City Codes: " + cities);
+        System.out.println("Phone number: " + phones);
+    }
+}
